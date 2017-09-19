c67b59a9e8a3a56ca639cbf860c3c0432ed3f9ad
lansdell <lansdell@uw.edu>
2017-09-19 19:20:05 -0400

    Update

--- lib/causal.py
+++ lib/causal.py
@@ -15,15 +15,15 @@ def causaleffect(v, Cost, p, params):
 def causaleffect_linear(v, Cost, p, params):
 	mu = params.mu;
 	mce = np.zeros(v.shape[0])
-	lr = linear_model()
+	lr = linear_model.LinearRegression()
 	for j in range(v.shape[0]):
 		abv = (v[j,:]>mu) & (v[j,:]<(mu+p))
 		blo = (v[j,:]<mu) & (v[j,:]>(mu-p))
 		C_abv = Cost[abv]
 		C_blo = Cost[blo]
-		lr.fit(v[j,abv], C_abv) 
+		lr.fit(v[j,abv].reshape((-1,1)), C_abv) 
 		beta_abv = lr.predict(mu)
-		lr.fit(v[j,blo], C_blo) 
+		lr.fit(v[j,blo].reshape((-1,1)), C_blo) 
 		beta_blo = lr.predict(mu)
 		mce[j] = beta_abv-beta_blo
 	return mce
