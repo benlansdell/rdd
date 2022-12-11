afc90213aaca6aab60b68bba06a49ce89ae06bd2
Ben Lansdell <ben.lansdell@stjude.org>
2022-12-11 04:07:35 -0600

    Add supplementary figure scripts/notebooks

---
+++ lib/paramsweep_w2_deltaT_counterfactual_refractory.py
@@ -0,0 +1,52 @@
+import numpy as np
+from lib.lif import LIF, ParamsLIF
+from lib.causal import causaleffect
+from tqdm import tqdm
+
+#Set x = 0, sigma = 10
+#wvals = 2..20
+sigma = 10
+mu = 1
+tau = 1
+t = 500
+
+#Simulate for a range of $W$ values.
+N = 19
+nsims = 1
+wmax = 20
+deltaT = 50
+
+#Play with different c values
+
+refractory_periods = [0.001, 0.003, 0.005]
+cvals = [0.5]
+
+for tr in refractory_periods:
+    for c in cvals:
+
+        params = ParamsLIF(sigma = sigma, mu = mu, tau = tau, tr = tr)
+        lif = LIF(params, t = t)
+        lif.x = 0
+        n = params.n
+
+        print("Running simulations for c = %f"%c)
+        outfile = './data/output/param_w_N_%d_nsims_%d_c_%f_tr_%f_deltaT_counterfactual_simulations.npz'%(N, nsims, c, tr)
+        params.c = c
+        lif.setup(params)
+        lif.x = 0
+        wvals = np.linspace(2, wmax, N)
+        vs = np.zeros((N, N, nsims, n, lif.T), dtype=np.float16)
+        hs = np.zeros((N, N, nsims, n, lif.T), dtype=np.bool)
+        us = np.zeros((N, N, nsims, n, lif.T), dtype=np.float16)
+        for i,w0 in tqdm(enumerate(wvals)):
+            for j,w1 in enumerate(wvals):
+                print("Running %d simulations with w0=%f, w1=%f"%(nsims, w0, w1))
+                lif.W = np.array([w0, w1])
+                for k in range(nsims):
+                    (v, h, Cost, betas, u) = lif.simulate(deltaT)
+                    vs[i,j,k,:] = v
+                    hs[i,j,k,:] = h
+                    us[i,j,k,:] = u
+        #Save output
+        np.savez(outfile, vs = vs, hs = hs, params = params, wvals = wvals\
+            , nsims = nsims, us = us)
